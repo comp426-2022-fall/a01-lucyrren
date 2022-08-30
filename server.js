8c3e3ab891e016afc3b6bdc473e1f59f4bb69724
Lucy <lucyren@email.unc.edu>
2022-08-30 15:06:47 -0400

    server console log statement

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = http.createServer((req, res) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
 server.listen(port, () => {
-    console.log(`Server running at port ${port}`)
+    console.log(`Server listening on port ${port}`)
   })
 });
 
